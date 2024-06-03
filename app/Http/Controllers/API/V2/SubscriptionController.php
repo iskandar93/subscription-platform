c620befb531ece07a4fed0a4e91e5faa5dfe4145
Iskandar Isman <iskandareizyh12@gmail.com>
2024-06-03 12:04:52 +0800

    feat: add subscription data information

---
+++ app/Http/Controllers/API/V2/SubscriptionController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\API\V2;
+
+use App\Models\Bill;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class SubscriptionController extends Controller
+{
+    public function index()
+    {
+        $data = Bill::all();
+
+        return response()->json([
+            'status' => 200,
+            'data' => $data
+        ]);
+    }
+}
